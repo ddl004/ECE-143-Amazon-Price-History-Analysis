689f7d8660d01e4036f726ccb2a7beabf9703784
Sai <skomatin@ucsd.edu>
2019-12-01 11:06:42 -0700

    main file added

---
+++ main.py
@@ -0,0 +1,77 @@
+from category_class import Category
+from product_class import Product
+
+import numpy as np
+from scipy import stats
+import datetime
+import matplotlib.dates as mdates
+from matplotlib import pyplot as plt
+from workalendar.usa import UnitedStates
+from operator import itemgetter
+import pandas as pd
+from math import pi
+
+def plot_radar(data):
+    
+    min_label = data.min().min() - 0.1
+    max_label = data.max().max() + 0.1
+    
+    N = data.count(0)[0]
+    theta = [n / float(N) * 2 * pi for n in range(N)]; theta += theta[:1]
+    
+    fig, axes = plt.subplots(figsize=(9, 9), nrows=2, ncols=2,
+                             subplot_kw=dict(polar=True))
+    fig.subplots_adjust(wspace=1.1, hspace=0.50, top=0.85, bottom=0.05)
+
+    colors = ['b', 'y', 'g', 'r']
+    # Plot the four cases from the example data on separate axes
+    for ax, hol, color in zip(axes.flat, data.items(), colors):
+        title = hol[0]
+        d = list(hol[1].values); d+= d[:1]
+
+        ax.set_rgrids([-0.2, -0.6, 0, 0.4])
+#         ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1), 
+#                      horizontalalignment='center', verticalalignment='center')
+        ax.plot(theta, d, color=color)
+        ax.fill(theta, d, facecolor=color, alpha=0.25)
+
+        ax.set_xticks(theta)
+        ax.set_xticklabels(data.index, {'color': 'black', 'size': 12})#TODO: Replace with smaller text
+        
+        ax.set_yticks([min_label, 0, max_label])#assuming max_label is greater than 0
+        ax.set_yticklabels(['%.1f' % min_label,"0", '%.1f' % max_label], {'color': 'grey', 'size': 9})
+        ax.set_ylim(min_label, max_label)
+        ax.legend([title], loc = (-0.55, -0.1))
+
+    fig.text(0.5, 0.965, 'Average Standardized Prices across Four Product Categories',
+             horizontalalignment='center', color='black', weight='bold',
+             size='large')
+    
+    plt.savefig('test.jpg')
+    plt.show()
+
+def plot_holiday(cat1, cat2, cat3, cat4):
+	_, hol_1 = cat1.holiday_correlation(2018, False)
+	_, hol_2 = cat2.holiday_correlation(2018, False)
+	_, hol_3 = cat3.holiday_correlation(2018, False)
+	_, hol_4 = cat4.holiday_correlation(2018, False)
+
+	frame = {'Office Products': hol_1, 'Software': hol_2, 'Electronics': hol_3, 'Toys': hol_4}
+	hol_df = pd.DataFrame(frame)
+	plot_radar(data = hol_df)
+
+if __name__ == "__main__":
+	products1 = list(np.load('office_products_sorted_ph.npy', allow_pickle=True))
+	products2 = list(np.load('software.npy', allow_pickle=True))
+	products3 = list(np.load('product_electronics_sorted_ph.npy', allow_pickle=True))
+
+	products1 = [Product(i) for i in products1]
+	products2 = [Product(i) for i in products2]
+	products3 = [Product(i) for i in products3]
+
+	cat1 = Category(products1)
+	cat2 = Category(products2)
+	cat3 = Category(products3)
+	cat4 = cat3#TODO: use toys data
+
+	plot_holiday(cat1, cat2, cat3, cat4)
